449729d41982b31547399603218fe6c02d3d528e
tujiahao <tujiahao0731@hotmail.com>
2021-01-19 17:30:56 +0100

    test DB

---
+++ app/src/main/java/com/example/uman_android_project/FormSubmitActivity.java
@@ -0,0 +1,13 @@
+package com.example.uman_android_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormSubmitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_submit);
+    }
+}
