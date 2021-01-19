529cdc8b8ccb1564834365b32ff030485ed01f03
tujiahao <tujiahao0731@hotmail.com>
2021-01-19 17:54:56 +0100

    try read

--- app/src/main/java/com/example/uman_android_project/FormSubmitActivity.java
+++ app/src/main/java/com/example/uman_android_project/FormSubmitActivity.java
@@ -1,13 +1,44 @@
 package com.example.uman_android_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
 
 public class FormSubmitActivity extends AppCompatActivity {
+
+    private static final String TAG = "FormActivity";
+    private TextView first, last, born;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_submit);
+
+        MainActivity.db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        CollectionReference users = MainActivity.db.collection("users");
     }
 }
--- app/src/main/java/com/example/uman_android_project/MainActivity.java
+++ app/src/main/java/com/example/uman_android_project/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.uman_android_project;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -16,13 +19,14 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    public static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
         // Create a new user with a first and last name
 
@@ -31,13 +35,14 @@ public class MainActivity extends AppCompatActivity {
         user.put("last", "Lovelace");
         user.put("born", 1815);
 
-// Add a new document with a generated ID
-        db.collection("users")
+        // Add a new document with a generated ID
+        /*db.collection("users")
                 .add(user)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
                         Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -45,7 +50,12 @@ public class MainActivity extends AppCompatActivity {
                     public void onFailure(@NonNull Exception e) {
                         Log.w(TAG, "Error adding document", e);
                     }
-                });
+                });*/
 
+    }
+
+    public void submit(View view) {
+        Intent intent = new Intent(MainActivity.this, FormSubmitActivity.class);
+        startActivity(intent);
     }
 }
