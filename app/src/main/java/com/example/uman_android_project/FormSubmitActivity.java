5d7d837600b3c3c42986a7e755a8e98540bdc989
xinyi-huang96 <xinyi.huang96@hotmail.com>
2021-01-20 23:44:26 +0100

    demo for showing existing data on firestore

--- app/src/main/java/com/example/uman_android_project/FormSubmitActivity.java
+++ app/src/main/java/com/example/uman_android_project/FormSubmitActivity.java
@@ -1,44 +1,52 @@
 package com.example.uman_android_project;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.Map;
+import java.util.List;
 
 public class FormSubmitActivity extends AppCompatActivity {
 
     private static final String TAG = "FormActivity";
     private TextView first, last, born;
+    private RecyclerView recyclerView;
+    private List<Tree> listTree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_submit);
 
-        MainActivity.db.collection("users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
+        recyclerView = findViewById(R.id.recycleview);
 
-                            }
-                        } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-        CollectionReference users = MainActivity.db.collection("users");
+        Query query = FirebaseFirestore.getInstance()
+                .collection("users")
+                .whereEqualTo("owner", "me");
+        query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Log.d("TAG", "Error :" + error.getMessage());
+                    return;
+                }
+                List<Tree> listTree = value.toObjects(Tree.class);
+                AdapterTree adapterTree = new AdapterTree(listTree);
+                recyclerView.setLayoutManager(new LinearLayoutManager(FormSubmitActivity.this));
+                recyclerView.setAdapter(adapterTree);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/uman_android_project/MainActivity.java
+++ app/src/main/java/com/example/uman_android_project/MainActivity.java
@@ -1,60 +1,41 @@
 package com.example.uman_android_project;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     public static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+    private EditText first, last, born;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-        // Create a new user with a first and last name
-
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-        // Add a new document with a generated ID
-        /*db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });*/
+        first = findViewById(R.id.firstInput);
+        last = findViewById(R.id.lastInput);
+        born = findViewById(R.id.bornInput);
 
     }
 
     public void submit(View view) {
+        String firststr = first.getText().toString();
+        String laststr = last.getText().toString();
+        String bornstr = born.getText().toString();
+
+        Tree tree = new Tree(firststr, laststr, bornstr, "me");
+
+        // Add a new document with a generated ID
+        db.collection("users").document().set(tree);
         Intent intent = new Intent(MainActivity.this, FormSubmitActivity.class);
         startActivity(intent);
     }
