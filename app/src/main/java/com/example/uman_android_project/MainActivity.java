82a550ca2306a4c753422fb0a760e2299f32d633
xinyi-huang96 <xinyi.huang96@hotmail.com>
2021-02-03 16:29:48 +0800

    gps works

--- app/src/main/java/com/example/uman_android_project/MainActivity.java
+++ app/src/main/java/com/example/uman_android_project/MainActivity.java
@@ -1,38 +1,130 @@
 package com.example.uman_android_project;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.DecimalFormat;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     public static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    private EditText first, last, born;
+    private EditText name, size, category, gps, date, comment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        first = findViewById(R.id.firstInput);
-        last = findViewById(R.id.lastInput);
-        born = findViewById(R.id.bornInput);
+        name = findViewById(R.id.nameInput);
+        size = findViewById(R.id.sizeInput);
+        category = findViewById(R.id.categoryInput);
+        gps = findViewById(R.id.gpsInput);
+        date = findViewById(R.id.dateInput);
+        comment = findViewById(R.id.commentInput);
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(false);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        String provider = locationManager.getBestProvider(criteria, true);
+        gps.setText(provider);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        });
+        Location location=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        locationUpdates(location);
+    }
+
+    public void locationUpdates(Location location){
+        DecimalFormat df = new DecimalFormat("#.00");
+        if(location!=null){
+            StringBuffer stringBuffer=new StringBuffer();
+            stringBuffer.append("longitude: ");
+            String longtitude = df.format(location.getLongitude());
+            if (longtitude.contains("-")) {
+                stringBuffer.append(longtitude.substring(1));
+                stringBuffer.append("S");
+            }
+            else {
+                stringBuffer.append(longtitude);
+                stringBuffer.append("N");
+            }
+            stringBuffer.append(" ,latitude: ");
+            String latitude = df.format(location.getLatitude());
+            Log.i(TAG, "locationUpdates: " + location.getLatitude());
+            if (latitude.contains("-")) {
+                stringBuffer.append(latitude.substring(1));
+                stringBuffer.append("W");
+            }
+            else {
+                stringBuffer.append(latitude);
+                stringBuffer.append("E");
+            }
+            Log.i(TAG, "locationUpdates: " + location.getLongitude());
+            gps.setText(stringBuffer.toString());
+        }else {
+            gps.setText("no.......");
+        }
 
     }
 
     public void submit(View view) {
-        String firststr = first.getText().toString();
-        String laststr = last.getText().toString();
-        String bornstr = born.getText().toString();
+        String namestr = name.getText().toString();
+        String sizestr = size.getText().toString();
+        String categorystr = category.getText().toString();
+        String gpsstr = gps.getText().toString();
+        String datestr = date.getText().toString();
+        String commentstr = comment.getText().toString();
 
-        Tree tree = new Tree(firststr, laststr, bornstr, "me");
+        SharedPreferences sharedPreferences = this.getSharedPreferences("user", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("id", "user1");
+        editor.commit();
+        String userid = sharedPreferences.getString("id", "notExist");
+
+
+        Tree tree = new Tree(namestr, sizestr, categorystr, gpsstr, datestr, commentstr, userid);
 
         // Add a new document with a generated ID
         db.collection("users").document().set(tree);
